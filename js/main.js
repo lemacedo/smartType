d65358e3bd897343aa9fe69b49c88581ac294082
unknown <leandromacedo12@gmail.com>
2015-02-16 03:15:48 -0200

    A criatura esta criando forma hahahahaaa

--- js/main.js
+++ js/main.js
@@ -1,32 +1,43 @@
 
 libLetras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z','w'];
-mapLetras = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91];
+
 
 function inicializa(){
- 
     canvas = document.getElementById("jogo");
     context = canvas.getContext("2d");
 
-    desafio = libLetras[geraPosicao(0,2)];
+    geraDesafio();  
+
+}
+
+function keyPressed(evt){
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(evt){
+    str = keyPressed(evt);
+
+    if( str == libLetras[posKey]){
+        // alert('Acertou');
+        gameLoop();
+        geraDesafio();
+    }else{
+        alert('Errou');
+    }
+}
+
+function geraDesafio(){
+    posKey = geraPosicao(0,26);
+    desafio = libLetras[posKey];
 
     var posX = geraPosicao(45,450);
     var posY = geraPosicao(45,450);
 
     exibeDesafio( desafio, posX, posY);
-
-    document.addEventListener('keydown', pegaTecla);
-
 }
 
-function pegaTecla(e){
-	var tecla = e.keyCode;
-
-	if(tecla == mapLetras[1] ){
-		alert('Acertou' + tecla + " == " + mapLetras[1]);
-	}else{
-		alert('Errou '  + tecla + " == " + mapLetras[1]);
-	}
-}
 
 function exibeDesafio(valor, posX, posY){
 	context.font = '40pt Calibre';
@@ -36,4 +47,11 @@ function exibeDesafio(valor, posX, posY){
 
 function geraPosicao(valMin, valMax){
 	return Math.floor( Math.random()  * valMax ) + valMin ;
+}
+
+function gameLoop(){
+
+    context.clearRect(0, 0, canvas.width+100, canvas.height+100);
+ 
+    
 }
